659e4b1dfd513909d680444bd4cafae0122138c1
nein-tanuki <neintanuki@gmail.com>
2022-02-10 13:46:15 +0800

    added validation to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
 	belongs_to :user
+
+	validates :title, presence: true, length: { maximum: 40 }, format: { without: /\A[\s]/ }
+	validates :description, format: { without: /\A[\s]/ }
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,21 +1,21 @@
 require "test_helper"
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "test" do
-  #   user = User.create(username: "hellloooo", password: "password123")
-  #   order = user.projects.create(title: "helo", description: "this is the body")
+  test "test" do
+    user = User.create(username: "hellloooo", password: "password123")
+    order = user.projects.create(title: "helo", description: "this is the body")
 
 
 
-  #   if order.save
-  #     puts order.user_id
-  #     puts user.id
-  #   else
-  #     puts 'fail'
-  #   end
+    if order.save
+      puts order.user_id
+      puts user.id
+    else
+      puts 'fail'
+    end
 
-  #   assert true
-  # end
+    assert true
+  end
 
   # test "the truth" do
   #   assert true
