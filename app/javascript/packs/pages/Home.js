3a44ee3670f8a67042a0bde2f7cbb70579efae0f
nein-tanuki <neintanuki@gmail.com>
2022-02-22 16:40:03 +0800

    added homepage

--- app/javascript/packs/pages/Home.js
+++ app/javascript/packs/pages/Home.js
@@ -3,15 +3,18 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div className="home">
-      <h1>Welcome to PlanUp</h1>
-      <p>Manage projects, catgories, and tasks using PlanUp. Sign up now</p>
+    <div className="home w-100 vh-100 d-flex justify-content-center align-items-center">
+      <div className="content container p-4 text-center">
+        <h1>Welcome to PlanUp</h1>
+        <p>Manage projects, catgories, and tasks using PlanUp. Sign up now</p>
 
-      <div className="btn-group">
-        <a href="">Sign Up</a>
-        <span>or</span>
-        <a href="">Log In</a>
+        <div className="btn-group d-flex align-items-center justify-content-center">
+          <Link to="/user/register" className="rounded-pill py-3 px-5 bg-info text-light">Sign Up</Link>
+          <span className="px-4 text-light">or</span>
+          <Link to="/user/login" className="rounded-pill py-3 px-5 bg-info text-light">Log In</Link>
+        </div>      
       </div>
+
     </div>
   )
 }
